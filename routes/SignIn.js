c6360de3320ad7fbfdfac72e114d62d1c524d085
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-05-20 00:27:28 +0530

    Signin returns object

--- routes/SignIn.js
+++ routes/SignIn.js
@@ -1,27 +1,33 @@
-const router=require('express').Router();
-const {User} =require('../db/models')
-const bcrypt =require('bcrypt')
+const router = require("express").Router();
+const { User } = require("../db/models");
+const bcrypt = require("bcrypt");
 
-router.post('/',(req,res)=>{
+getAuthTokenId = () => {
+  return "Hi";
+};
 
-  const {email,password}=req.body;
+router.post("/", (req, res) => {
+  const { email, password } = req.body;
+
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    return getAuthTokenId();
+  }
 
   User.findOne({
     where: {
       email: email
     }
-  }).then((user)=>{
-    
+  }).then(user => {
     bcrypt.compare(password, user.password, function(err, result) {
-      
-      res.send(result);
-
+      if (result === true) {
+        res.send({ success: true });
+      } else {
+        res.send({ success: false });
+      }
     });
-    
   });
+});
 
-  
-
-})
-
-module.exports=router
+module.exports = router;
