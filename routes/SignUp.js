24155f059ebecee5b94e37683238b2fcff065c42
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-05-19 21:01:22 +0530

    Simple db fecth

--- routes/SignUp.js
+++ routes/SignUp.js
@@ -14,6 +14,8 @@ router.post('/',(req,res)=>{
 
   const {firstName,email,password}=req.body;
 
+  console.log(firstName);
+
   bcrypt.hash(password,saltRounds,(err,hash)=>{
     User.create({
       firstName:firstName,
