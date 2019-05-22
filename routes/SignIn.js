924ae6f72e7ef4bd65f1a0ed6611d54ca246bad1
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-05-23 00:40:05 +0530

    Added redis

--- routes/SignIn.js
+++ routes/SignIn.js
@@ -2,19 +2,33 @@ const router = require("express").Router();
 const { User } = require("../db/models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const redis = require("redis");
+const protobuf = require("protobufjs");
 
-getAuthTokenId = () => {
-  return "Hi";
+// Setup redis
+const redisClient = redis.createClient();
+
+getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(400).json("Unauthorized");
+    } else {
+      return res.json({ id: reply });
+    }
+  });
 };
 
 const signToken = email => {
   const jwtPayload = { email };
   return jwt.sign(jwtPayload, "JWT_SECRET");
 };
+
 const createSession = user => {
   // JWT Token and return user data
   const { email, user_id } = user;
   const token = signToken(email);
+  redisClient.set(token, user_id);
   return { success: true, user_id: user_id, token: token };
 };
 
@@ -24,7 +38,7 @@ router.post("/", (req, res) => {
   const { authorization } = req.headers;
 
   if (authorization) {
-    return getAuthTokenId();
+    return getAuthTokenId(req, res);
   }
 
   User.findOne({
@@ -35,7 +49,7 @@ router.post("/", (req, res) => {
     bcrypt.compare(password, user.password, function(err, result) {
       if (result === true) {
         const data = createSession(user);
-        res.send(data);
+        res.json(data);
       } else {
         res.send({ success: false });
       }
